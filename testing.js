f938931add028adb521041b8d8ed931dbc186e38
Casey Segura <caseyrseguramgmt@gmail.com>
2025-03-12 17:20:22 -0400

    rebasing cctest

--- testing.js
+++ testing.js
@@ -6,11 +6,20 @@
 //git rebase main
 // git push -f
 
-
-
 //making a branch
 //git checkout -b branchName 
 //git add .
 //git commit -m "message"
 //git push origin branchName
+
+//git rebase steps
 //git checkout main
+//git pull
+//git checkout branch
+//git rebase main
+//git push -f
+//git checkout main
+//git pull
+//git checkout branch
+//git rebase main
+//git push -f
