b8147873caaae65745414215e68028260ea4ba9f
Casey Segura <caseyrseguramgmt@gmail.com>
2025-04-29 03:44:26 -0400

    rebase tips

--- testing.js
+++ testing.js
@@ -14,3 +14,41 @@
 //git commit -m "message"
 //git push origin branchName
 //git checkout main
+
+/*
+// on vscode
+// steps to rebasing
+//git checkout -b branchName
+//git add .
+//git commit -m "message"
+//git push origin branchName
+
+// on github
+//go to pull request
+//click on pull request
+//click on compare
+//click on create pull request
+//click on merge
+
+// on new branch in vscode
+// git checkout -b newBranch
+// git add .
+// git commit -m "message"
+// git push origin newBranch
+
+// on github
+// go to pull request
+// click on pull request
+// click on compare
+// click on create pull request
+// resolve conflicts
+
+// on vscode
+// git checkout main
+// git pull
+// git checkout branchName
+// git rebase main
+// git rebase --continue
+// git push -f
+
+*/
